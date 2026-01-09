e6188c89802a5d714180b3f9b949836d1956f902
jinxedValerie <jinxedValerie@gmail.com>
2026-01-09 17:30:39 +0100

    fixes

--- UART/test_encode.py
+++ UART/test_encode.py
@@ -11,18 +11,15 @@ message_input=input()
 
 def new_letter_fn(letter, case_offset, key):                       #97 lower, 65 upper 
     letter_num = ord(letter) - case_offset
-    new_num = ((letter_num + key) % 26) +case_offset
+    new_num = ((letter_num + key) % 26) + case_offset
     new_letter = chr(new_num)
     return new_letter
 
 
-
-
-def encoder(message_in, key):
-    message = message_in
+def encoder(message: str, key: int) -> str:
     new_word = ""
     for letter in message:
-        if ord(letter)==32:             #Leerzeichen
+        if letter == " ":             #Leerzeichen
              new_letter = " "
         elif letter.islower():
              new_letter= new_letter_fn(letter, 97, key)
@@ -32,19 +29,11 @@ def encoder(message_in, key):
     return new_word
 
 
+
 def decoder(message_in, key):
-    message = message_in
-    new_word = ""
-    key_rev= -1 * key
-    for letter in message:
-        if ord(letter)==32:             #Leerzeichen
-             new_letter = " "
-        elif letter.islower():
-             new_letter= new_letter_fn(letter, 97, key_rev)
-        else:
-             new_letter= new_letter_fn(letter,65, key_rev)
-        new_word += new_letter
-    return new_word
+    return encoder(message_in, -1 * key)
 
-
-print(encoder(message_input,key))
+encoded = encoder(message_input,key)
+print(encoded)
+decoded = decoder(encoded, key)
+print(decoded)

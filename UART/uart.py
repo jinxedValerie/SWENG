247dc9132e1769a9f11e9570230742bbda274833
jinxedValerie <jinxedValerie@gmail.com>
2026-01-09 19:43:46 +0100

    remove debouncing logic

--- UART/uart.py
+++ UART/uart.py
@@ -1,8 +1,5 @@
-import machine
 from machine import UART, Pin, idle
 import micropython
-from micropython import schedule
-from time import ticks_ms, ticks_diff
 
 micropython.alloc_emergency_exception_buf(100)
 
@@ -14,15 +11,6 @@ BTN = Pin(15, Pin.IN, Pin.PULL_UP)
 CON = UART(0, baudrate=9600, bits=8, parity=None, stop=2, rx=RX_PIN, tx=TX_PIN)
 
 
-# def read_transmission(rx: UART):
-#     bytes = rx.read()
-
-# def tx_detected():
-#     schedule(handle_tx, None)
-
-# con.irq(read_transmission, UART.IRQ_RXIDLE, False)
-
-
 def caesar_cipher(input: str, shift: int) -> str:
     message = ""
     for c in input.upper():
@@ -32,24 +20,12 @@ def caesar_cipher(input: str, shift: int) -> str:
         message += chr(((ord(c) - ord("A") + shift) % 26) + ord("A"))
     return message
 
-def send_message(_: None):
+def send_message(_: Pin) -> None:
     MESSAGE = "HELLO WORLD!"
     sent_bytes = CON.write(MESSAGE)
     print(f"Sent message: {MESSAGE} ({sent_bytes})")
 
-DEBOUNCE_MARGIN = 100
-last_button_press = ticks_ms()
-def handle_button_press(pin: Pin):
-    irq_state = machine.disable_irq()   # disable IRQs to prevent multiple repeated calls
-
-    global last_button_press
-    if (ticks_diff(ticks_ms(), last_button_press) > DEBOUNCE_MARGIN):
-        last_button_press = ticks_ms()
-        schedule(send_message, None)
-        
-    machine.enable_irq(irq_state)   # reenable IRQs
-
-BTN.irq(handler=handle_button_press, trigger=Pin.IRQ_RISING, hard=False)
+BTN.irq(handler=send_message, trigger=Pin.IRQ_FALLING, hard=False)
 
 print("RUNNING")
 

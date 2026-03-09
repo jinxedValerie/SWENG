2d2c8bd69a6084f4dfe87ed2946f14a867faa405
MarvinRH <hornmarvin01@gmail.com>
2026-03-09 22:41:24 +0100

    Achtung fehlerhaft

--- Maze/map.py
+++ Maze/map.py
@@ -10,11 +10,12 @@ class Map:
             for y, row in enumerate(file):
                 row_clean = row.strip()
                 rowlist = []
-                if any(["." in data_row for data_row in file]):
-                    for x, char in enumerate(row_clean):
+                for x, char in enumerate(row_clean):
+                    if any(["." in data_row for data_row in file]):
+                    
                         if char == ".":
                             rowlist.append("Path")
-                        elif char =="W":
+                        elif char == "W":
                             rowlist.append("Wall")
                         elif char == "S":
                             rowlist.append("Path")
@@ -24,13 +25,12 @@ class Map:
                             self.finish = (x,y)
                         elif char.isdigit():
                             rowlist.append("Path")
-                            self.coins.append((x,y,int(char)))
-                    self.gridlist.append(rowlist)
-                else:
-                    for x, char in enumerate(row_clean):
+                            self.coins.append(((x,y),int(char)))
+                    else:
+                   
                         if char == "W":
                             rowlist.append("Path")
-                        elif char =="X":
+                        elif char == "X":
                             rowlist.append("Wall")
                         elif char == "S":
                             rowlist.append("Path")
@@ -40,14 +40,18 @@ class Map:
                             self.finish = (x,y)
                         elif char.isdigit():
                             rowlist.append("Path")
-                            self.coins.append((x,y,int(char)))
-                    self.gridlist.append(rowlist)
+                            self.coins.append(((x,y),int(char)))
+                self.gridlist.append(rowlist)
 
 
     def size(self):
         return (len(self.gridlist[0]),len(self.gridlist))
 
-    def check_coordinate(self,x,y):
+    def check_coordinate(self,position):
+        x= position[0]
+        y= position[1]
+        if x < 0 or x >= len(self.gridlist[0]) or y < 0 or y >= len(self.gridlist):
+            return "Wall"
         return self.gridlist[y][x]
     
     def get_start(self):
@@ -58,15 +62,24 @@ class Map:
     
     def get_coins(self):
         return self.coins
-        
-
+    
 def self_test():
-    testmap = Map("./mazes/test-maze.txt")
+    testmap = Map(r"SWENG\Maze\mazes\test-maze.txt")
+
+    grid = testmap.gridlist
+    #assert grid ==
+
     size = testmap.size()
+    #assert size == (3,3)
+
     start = testmap.get_start()
+    #assert start == (0,0)
     finish = testmap.get_finish()
+    #assert finish == (0,2)
     coins = testmap.get_coins()
-    checkedcords = testmap.check_coordinate(2,2)
+    #assert coins == ((1,1),1)
+    checkedcords = testmap.check_coordinate((2,2))
+    #assert checkedcords == "Wall"
     return size,checkedcords,start,finish,coins,grid
 
 if __name__ == '__main__':

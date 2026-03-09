28fd6e314d5ae0d7bec6e39b6105440ea7d55292
Anna <degen.anna@t-online.de>
2026-03-09 19:33:54 +0100

    add test

--- Maze/player.py
+++ Maze/player.py
@@ -1,4 +1,5 @@
 from map import Map
+from time import sleep
 
 class Player:
 
@@ -21,23 +22,11 @@ class Player:
             self.current_position = spot
             return None
         
-        self.view = (self.view - 180) % 360                                            #look left
-        spot= find_spot(self.view, self.current_position)
-        if self.current_map.check_coordinate(spot[0],spot[1]) == "Path":
-            self.current_position = spot
-            return None
-        
-        self.view = (self.view + 90) % 360                                             #look straight
-        spot= find_spot(self.view, self.current_position)
-        if self.current_map.check_coordinate(spot[0],spot[1]) == "Path":
-            self.current_position = spot
-            return None
-        
-        self.view = (self.view - 180) % 360                                             #look back
-        spot= find_spot(self.view, self.current_position)
-        if self.current_map.check_coordinate(spot[0],spot[1]) == "Path":
-            self.current_position = spot
-            return None
+        while self.current_map.check_coordinate(spot[0],spot[1]) != "Path":
+            self.view = (self.view - 90) % 360  
+            spot= find_spot(self.view, self.current_position)
+        self.current_position=spot
+           
 
     def seen_pos(self):
         return self.visited_pos
@@ -62,83 +51,18 @@ def find_spot(view, position):
     
     return spot
 
-
 def test():
-
-    #W W W
-    #W p .
-    #W W W
-
-    current_map = (("Wall", "Wall" , "Wall") , ("Wall","Path", "Path") , ("Wall", "Wall" , "Wall"))
-    current_position = (1,1)
-    visited_position= set()
-    view = 0                           #start looking down
-
-    def position():  
-        return current_position
-
-    def move(view,current_position):
-        spot= (0,0)
-        visited_position.add(current_position)
-
-        view = (view + 90) % 360  
-        print(view)                                            #look right
-        spot= find_spot(view, current_position)
-        if check_coordinate(spot[0],spot[1]) == "Path":
-            current_position = spot
-            return current_position
-        
-        view = (view - 180) % 360                              #look left
-        print(view)                                       
-        spot= find_spot(view, current_position)
-        if check_coordinate(spot[0],spot[1]) == "Path":
-            current_position = spot
-            return current_position
-        
-        view = (view + 90) % 360                                             #look straight
-        print(view)                                       
-        spot= find_spot(view, current_position)
-        if check_coordinate(spot[0],spot[1]) == "Path":
-            current_position = spot
-            return current_position
-        
-        view = (view - 180) % 360                                             #look back
-        print(view)                                       
-        spot= find_spot(view, current_position)
-        if check_coordinate(spot[0],spot[1]) == "Path":
-            current_position = spot
-            return current_position
-
-    def seen_pos():
-
-    def check_coordinate(x,y):
-        row = current_map[y]
-        place = row[x]
-        if place == "Path": return "Path"
-        else: return "Wall"
+    map_test= Map(r"Maze/mazes/Labyrinth-2.txt")
+    player = Player(map_test)
+    print(player.position())
+    print(map_test.get_finish())
+    while player.position() != map_test.get_finish():
+        player.move()
+        print(player.position())
+        sleep(0.2)
+    print(player.seen_pos())
+    print(player.current_position)
 
 
-    def find_spot(view, position):
-        x= position[0]
-        y= position[1]
-        if view == 90:
-            spot=(x - 1 , y)
-
-        if view == 0:
-            spot = (x , y + 1)
-
-        if view == 270:
-            spot = (x + 1, y)
-
-        if view == 180:
-            spot= (x , y - 1) 
-
-        return spot
-
-
-    def main():
-        print(current_position)
-        print(move(view, current_position))
-
-
-    main()
+if __name__ == '__main__':
+    test()

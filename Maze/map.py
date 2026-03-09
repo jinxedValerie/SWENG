2b9770c1af3ef5c07bfa110a1c911eef7a7bade6
MarvinRH <hornmarvin01@gmail.com>
2026-03-09 21:38:21 +0100

    format unterscheidung

--- Maze/map.py
+++ Maze/map.py
@@ -10,22 +10,39 @@ class Map:
             for y, row in enumerate(file):
                 row_clean = row.strip()
                 rowlist = []
-                for x, char in enumerate(row_clean):
-                    if char == ".":
-                        rowlist.append("Path")
-                    elif char =="W":
-                        rowlist.append("Wall")
-                    elif char == "S":
-                        rowlist.append("Path")
-                        self.start = (x,y)
-                    elif char == "Z":
-                        rowlist.append("Path")
-                        self.finish = (x,y)
-                    elif char.isdigit():
-                        rowlist.append("Path")
-                        self.coins.append((x,y,int(char)))
-                self.gridlist.append(rowlist)
-        
+                if any(["." in data_row for data_row in file]):
+                    for x, char in enumerate(row_clean):
+                        if char == ".":
+                            rowlist.append("Path")
+                        elif char =="W":
+                            rowlist.append("Wall")
+                        elif char == "S":
+                            rowlist.append("Path")
+                            self.start = (x,y)
+                        elif char == "Z":
+                            rowlist.append("Path")
+                            self.finish = (x,y)
+                        elif char.isdigit():
+                            rowlist.append("Path")
+                            self.coins.append((x,y,int(char)))
+                    self.gridlist.append(rowlist)
+                else:
+                    for x, char in enumerate(row_clean):
+                        if char == "W":
+                            rowlist.append("Path")
+                        elif char =="X":
+                            rowlist.append("Wall")
+                        elif char == "S":
+                            rowlist.append("Path")
+                            self.start = (x,y)
+                        elif char == "Z":
+                            rowlist.append("Path")
+                            self.finish = (x,y)
+                        elif char.isdigit():
+                            rowlist.append("Path")
+                            self.coins.append((x,y,int(char)))
+                    self.gridlist.append(rowlist)
+
 
     def size(self):
         return (len(self.gridlist[0]),len(self.gridlist))
@@ -45,11 +62,13 @@ class Map:
 
 def self_test():
     testmap = Map(r"SWENG\Maze\mazes\test-maze.txt")
+    grid = testmap.gridlist
     size = testmap.size()
     start = testmap.get_start()
     finish = testmap.get_finish()
     coins = testmap.get_coins()
-    return size,start,finish,coins
+    checkedcords = testmap.check_coordinate(2,2)
+    return size,checkedcords,start,finish,coins,grid
 
 if __name__ == '__main__':
     print(self_test())

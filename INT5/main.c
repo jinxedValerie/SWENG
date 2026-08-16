e1b1e5771f2013c7570091a06f82acda1c05e29a
jinxedValerie <jinxedValerie@gmail.com>
2026-08-16 20:56:58 +0200

    third oneeee

--- INT5/main.c
+++ INT5/main.c
@@ -2,6 +2,11 @@
 
 const unsigned int SAMPLE_FREQ = 16000;
 
+#define ANZ_HIST_KAN 4
+const int HIST_CLASS_1 = SAMPLE_FREQ / 1000;
+const int HIST_CLASS_2 = SAMPLE_FREQ / 100;
+const int HIST_CLASS_3 = SAMPLE_FREQ / 10;
+
 typedef enum
 {
     OK = 0,
@@ -75,12 +80,31 @@ void zero_crossings(short *samples, size_t length, short threshold, size_t *cros
     *crossings_idxs_len = crossings;
 }
 
-// diff_buf needs to be at least (length - 1) * sizeof(short) large
+// diff_buf needs to be at least `(length - 1) * sizeof(short)` large
 void differentiate_array(short array[], size_t length, short *diff_buf)
 {
     for (size_t i = 0; i < length - 1; i++)
     {
         diff_buf[i] = array[i + 1] - array[i];
+    }
+}
+
+// hist_buf needs to be at least `ANZ_HIST_KAN * sizeof(int)` large
+// hist buf needs to be zero-initialized or will add otherwise
+void crossing_histogram(size_t *crossing_idxs, size_t crossings, float *hist_buf)
+{
+    for (size_t i = 0; i < crossings - 1; i++)
+    {
+        int distance = crossing_idxs[i + 1] - crossing_idxs[i];
+
+        if (distance < HIST_CLASS_1)
+            hist_buf[0] += 1;
+        else if (distance < HIST_CLASS_2)
+            hist_buf[1] += 1;
+        else if (distance < HIST_CLASS_3)
+            hist_buf[2] += 1;
+        else
+            hist_buf[3] += 1;
     }
 }
 
@@ -146,7 +170,7 @@ Status signal_detekt(short **sample_anfang, unsigned int *sample_anzahl)
     int DC_level = mean(signal_samples, signal_length);
     for (size_t i = 0; i < signal_length; i++)
     {
-        signal_samples[i] = signal_samples[i] - DC_level;
+        signal_samples[i] -= DC_level;
     }
 
     // ESTABLISH RETURN STATE
@@ -190,11 +214,42 @@ Status ndg_dichte(short *feld_ptr, unsigned int anzahl_atw, float *dichte_ori, f
     return OK;
 }
 
+Status ndg_histogramm(short *feld_ptr, unsigned int anzahl_atw, float *hist_ori, float *hist_diff)
+{
+    // SETUP
+    size_t *crossings_buf = malloc(anzahl_atw * sizeof(size_t));
+    if (crossings_buf == nullptr)
+        return ERROR;
+    size_t crossings;
+
+    // ZERO OUT IN-HIST-BUFS
+    for (size_t i = 0; i < ANZ_HIST_KAN; i++)
+    {
+        hist_ori[i] = 0;
+        hist_diff[i] = 0;
+    }
+
+    // CALC ORI
+    zero_crossings(feld_ptr, anzahl_atw, NOISE_THRESHOLD, crossings_buf, &crossings);
+    crossing_histogram(crossings_buf, crossings, hist_ori);
+
+    // CALC DIFF
+    size_t diff_buf_len = anzahl_atw - 1;
+    short *diff_buf = malloc(diff_buf_len * sizeof(short));
+    if (crossings_buf == nullptr)
+    {
+        free(crossings_buf);
+        return ERROR;
+    }
+
+    differentiate_array(feld_ptr, anzahl_atw, diff_buf);
+    zero_crossings(diff_buf, diff_buf_len, NOISE_THRESHOLD, crossings_buf, &crossings);
+    crossing_histogram(crossings_buf, crossings, hist_diff);
+
+    return OK;
+}
+
 int main()
 {
     return 0;
 }
-
-// Status ndg_histogramm(short *feld_ptr, unsigned int anzahl_atw, float *hist_ori, float *hist_diff)
-// {
-// }

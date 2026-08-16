5ba353b1be6f98f309fe47db515df25f3aecf7a6
jinxedValerie <jinxedValerie@gmail.com>
2026-08-16 20:22:17 +0200

    second module i think

--- INT5/main.c
+++ INT5/main.c
@@ -46,6 +46,44 @@ int array_max(int array[], size_t length)
     return max;
 }
 
+void zero_crossings(short *samples, size_t length, short threshold, size_t *crossing_idxs, size_t *crossings_idxs_len)
+{
+    int current_state = 0; // 1: above threshold, -1: below threshold
+    size_t crossings = 0;
+
+    for (size_t i = 0; i < length; i++)
+    {
+        if (samples[i] > threshold)
+        {
+            if (current_state == -1)
+            {
+                crossing_idxs[crossings] = i;
+                crossings++;
+            }
+            current_state = 1;
+        }
+        else if (samples[i] < -threshold)
+        {
+            if (current_state == 1)
+            {
+                crossing_idxs[crossings] = i;
+                crossings++;
+            }
+            current_state = -1;
+        }
+    }
+    *crossings_idxs_len = crossings;
+}
+
+// diff_buf needs to be at least (length - 1) * sizeof(short) large
+void differentiate_array(short array[], size_t length, short *diff_buf)
+{
+    for (size_t i = 0; i < length - 1; i++)
+    {
+        diff_buf[i] = array[i + 1] - array[i];
+    }
+}
+
 const unsigned int ENERGY_BLUR = SAMPLE_FREQ / 100;              // UNSURE WHAT TO USE
 const float ENERGY_RELATIVE_THRESHOLD = 0.25f;                   // UNSURE WHAT TO USE
 const unsigned int ENERGY_DURATION_THRESHOLD = SAMPLE_FREQ / 10; // UNSURE WHAT TO USE
@@ -117,14 +155,45 @@ Status signal_detekt(short **sample_anfang, unsigned int *sample_anzahl)
     return OK;
 }
 
+const int NOISE_THRESHOLD = 50; // UNSURE WHAT TO USE
+Status ndg_dichte(short *feld_ptr, unsigned int anzahl_atw, float *dichte_ori, float *dichte_diff)
+{
+    // SETUP
+    size_t *crossings_buf = malloc(anzahl_atw * sizeof(size_t));
+    if (crossings_buf == nullptr)
+        return ERROR;
+    size_t crossings;
+
+    // CALC ORIGINAL
+    zero_crossings(feld_ptr, anzahl_atw, NOISE_THRESHOLD, crossings_buf, &crossings);
+
+    *dichte_ori = (float)crossings / (float)anzahl_atw;
+
+    // CALC DIFFERNTIATED
+
+    size_t diff_buf_len = anzahl_atw - 1;
+    short *diff_buf = malloc(diff_buf_len * sizeof(short));
+    if (diff_buf == nullptr)
+    {
+        free(crossings_buf);
+        return ERROR;
+    }
+    differentiate_array(feld_ptr, anzahl_atw, diff_buf);
+
+    zero_crossings(diff_buf, diff_buf_len, NOISE_THRESHOLD, crossings_buf, &crossings);
+
+    *dichte_diff = (float)crossings / (float)diff_buf_len;
+
+    free(diff_buf);
+    free(crossings_buf);
+
+    return OK;
+}
+
 int main()
 {
     return 0;
 }
-
-// Status ndg_dichte(short *feld_ptr, unsigned int anzahl_atw, float *dichte_ori, float *dichte_diff)
-// {
-// }
 
 // Status ndg_histogramm(short *feld_ptr, unsigned int anzahl_atw, float *hist_ori, float *hist_diff)
 // {
